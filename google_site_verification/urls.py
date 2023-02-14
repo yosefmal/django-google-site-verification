a0f92de4f1fc4041b0c358055ace29464f745be1
yosefmal <37828723+yosefmal@users.noreply.github.com>
2023-02-14 11:57:59 +0200

    Update urls.py
    update for django 4

--- google_site_verification/urls.py
+++ google_site_verification/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views import google_verification_view
 
 urlpatterns = [
-    url(r'^$', google_verification_view, name='google_verification_view'),
+    re_path(r'^$', google_verification_view, name='google_verification_view'),
 ]
